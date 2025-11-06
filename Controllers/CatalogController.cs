052e9617e489562fd49535321cf1f6fa534d8c93
Francisco Barber Otero <franciscobarber@gmail.com>
2025-11-06 19:31:25 +0000

    add

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using RetailDemo.Data;
 using RetailDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace RetailDemo.Controllers
 {
@@ -9,15 +14,33 @@ namespace RetailDemo.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly RetailDbContext _context;
-        public CatalogController(RetailDbContext context)
+        private readonly IMemoryCache _cache;
+        private const string ProductsCacheKey = "ProductsList";
+
+        public CatalogController(RetailDbContext context, IMemoryCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task<IActionResult> GetProducts()
         {
-            return Ok(_context.Products.ToList());
+            // Try to get the list of products from the cache
+            if (!_cache.TryGetValue(ProductsCacheKey, out List<Product> products))
+            {
+                // If the products are not in the cache, get them from the database
+                products = await _context.Products.ToListAsync();
+
+                // Configure cache options
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5)) // Keep in cache, reset expiration on access
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(30)); // Remove from cache after this time
+
+                // Save the data in the cache
+                _cache.Set(ProductsCacheKey, products, cacheEntryOptions);
+            }
+            return Ok(products);
         }
     }
 }
