95fe884b833a4eff5d994a57beff7223c572376f
Francisco Barber Otero <franciscobarber@gmail.com>
2025-11-07 22:59:00 +0000

    add

--- Migrations/20251107225846_SpecifyOrderItemPricePrecision.Designer.cs
+++ Migrations/20251107225846_SpecifyOrderItemPricePrecision.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailDemo.Data;
 
@@ -11,9 +12,11 @@ using RetailDemo.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(RetailDbContext))]
-    partial class RetailDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251107225846_SpecifyOrderItemPricePrecision")]
+    partial class SpecifyOrderItemPricePrecision
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251107225846_SpecifyOrderItemPricePrecision.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecifyOrderItemPricePrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CartId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Orders");
+        }
+    }
+}
